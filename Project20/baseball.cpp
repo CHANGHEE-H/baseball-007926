38f16681734bdd969f80a44547f760353b166ffe
changhee <jaduwvad2@gmail.com>
2025-05-02 16:11:16 +0900

    baseball game tdd 2

--- Project20/baseball.cpp
+++ Project20/baseball.cpp
@@ -5,7 +5,15 @@
 using namespace std;
 
 class Baseball {
-	void guess(const string& string) {
-		throw length_error("Must be three letters");
+public:
+	void guess(const string& str) {
+		if (3 != str.length()) {
+			throw length_error("Must be three letters");
+		}
+		for (char ch : str) {
+			if ('0' > ch || '9' < ch) {
+				throw invalid_argument("Must be number");
+			}
+		 }
 	}
 };
--- Project20/baseball_test.cpp
+++ Project20/baseball_test.cpp
@@ -1,10 +1,23 @@
 #include "baseball.cpp"
 #include "gmock/gmock.h"
 
-
-TEST(BaseballGame, ThrowLengthErrorException) {
+class BaseballFixture : public testing::Test {
+public:
 	Baseball game;
-	EXPECT_THROW(game.guess(string("12")), length_error)
+	void assertIllegalArgument(string guessNumber) {
+		try {
+			game.guess(string(guessNumber));
+			FAIL();
+		}
+		catch (exception e) {
+
+		}
+	}
+};
+
+TEST_F(BaseballFixture, ThrowInvalidCaseException) {
+	assertIllegalArgument("12");
+	assertIllegalArgument("12s");
 }
 
 int main() {
