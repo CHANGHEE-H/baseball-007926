07289918f29105ec9140ed9402ca16e093d9fb96
changhee <jaduwvad2@gmail.com>
2025-05-02 16:20:58 +0900

    baseball game tdd 3

--- Project20/baseball.cpp
+++ Project20/baseball.cpp
@@ -6,14 +6,28 @@ using namespace std;
 
 class Baseball {
 public:
-	void guess(const string& str) {
-		if (3 != str.length()) {
+	void guess(const string& guessNum) {
+		assertIllegalArgument(guessNum);
+	}
+private:
+	bool isDup(const string& guessNum) {
+		return (guessNum[0] == guessNum[1] ||
+			guessNum[1] == guessNum[2] ||
+			guessNum[0] == guessNum[2]);
+	}
+
+	void assertIllegalArgument(const std::string& guessNum)
+	{
+		if (3 != guessNum.length()) {
 			throw length_error("Must be three letters");
 		}
-		for (char ch : str) {
+		for (char ch : guessNum) {
 			if ('0' > ch || '9' < ch) {
 				throw invalid_argument("Must be number");
 			}
-		 }
+		}
+		if (true == isDup(guessNum)) {
+			throw invalid_argument("Must not have the same number.");
+		}
 	}
 };
